08e85b3720400b8c753805be76472ca9dca89124
WebertonSilva <weberton.fsilva@gmail.com>
2024-02-13 20:43:33 -0300

    Ajuste Model Transport

--- app/model/model.py
+++ app/model/model.py
@@ -1,8 +1,34 @@
 import uuid
 from typing import List
 
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, SerializeAsAny, Field
 
+
+
+
+
+
+
+# class Produto :
+#     def __init__(id, quantidade, descricao):
+#         id: str = Field(...)
+#         quantidade : int = Field(...)
+#         descricao : str = Field(...)
+#
+# class Transport :
+#     def __init__(transport_id, orderid, status, transport_value, list_of_products):
+#         transport_id: str = Field(default_factory=uuid.uuid4, alias="transport_id")
+#         orderid: str = Field(...)
+#         status: str = Field(...)
+#         transport_value: float = Field(...)
+#         list_of_products = list_of_products
+
+
+
+class Produto(BaseModel):
+    id: str = Field(...)
+    quantidade : int = Field(...)
+    descricao : str = Field(...)
 
 class Adress(BaseModel):
     street: str = Field(...)
@@ -12,22 +38,14 @@ class Adress(BaseModel):
     state : str = Field(...)
     cep: int = Field(...)
 
-
-class Produto(BaseModel):
-    id: str = Field(...)
-    quantidade : int = Field(...)
-    descricao : str = Field(...)
-
-
 class Transport(BaseModel):
 
-    # transport_id: str
     transport_id: str = Field(default_factory=uuid.uuid4, alias="transport_id")
     orderid: str = Field(...)
-    status : str = Field(...)
-    transport_value : float = Field(...)
-    list_produtos : List[Produto] = Field(...)
-    adress : Adress
+    status: str = Field(...)
+    transport_value: float = Field(...)
+    list_id_products: List[str] = []
+    #list_produtos: SerializeAsAny == List[Produto] = Field(...)
 
     # class Config:
     #     allow_population_by_field_name = True
